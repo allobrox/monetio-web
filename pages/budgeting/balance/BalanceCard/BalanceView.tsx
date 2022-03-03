9b7c82769c13d688328e0a9d6607dafb06e59f68
Tamas Rigoczki <rigoczkitamas@gmail.com>
2022-03-03 21:27:26 +0100

    MON-8: Added basic MonthSelector

--- pages/budgeting/balance/BalanceCard/BalanceView.tsx
+++ pages/budgeting/balance/BalanceCard/BalanceView.tsx
@@ -5,19 +5,26 @@ import { IncomeAccordion } from "../IncomeCard/IncomeAccordion";
 import { SpendingAccordion } from "../SpendingAccordion";
 import { SavingsAccordion } from "../SavingsAccordion";
 import { DebtAccordion } from "../DebtAccordion";
+import { MonthSelector } from "../../../../components/MonthSelector";
 
 export const BalanceView = () => {
     return (
-        <div className={styles.balance_view}>
-            <div className={styles.balance_container}>
-                <Accordion defaultActiveKey="0">
-                    <IncomeAccordion />
-                    <SpendingAccordion />
-                    <SavingsAccordion />
-                    <DebtAccordion />
-                </Accordion>
+        <div className={styles.balance_main}>
+            <div className={styles.menu_item}>
+                <MonthSelector />
             </div>
-            <AddTransactionCard />
+
+            <div className={styles.balance_view}>
+                <div className={styles.balance_container}>
+                    <Accordion defaultActiveKey="0">
+                        <IncomeAccordion />
+                        <SpendingAccordion />
+                        <SavingsAccordion />
+                        <DebtAccordion />
+                    </Accordion>
+                </div>
+                <AddTransactionCard />
+            </div>
         </div>
     );
 };
