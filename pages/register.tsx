953d66a925431664953bf15f11074d4f5d5d4194
Tamas Rigoczki <rigoczkitamas@gmail.com>
2022-03-27 09:31:27 +0200

    MON-11: Registration WIP

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,5 +1,7 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import { Button, Card, Form } from "react-bootstrap";
+import styles from "../styles/Register.module.css";
 
 const Register: NextPage = () => {
     return (
@@ -12,8 +14,41 @@ const Register: NextPage = () => {
                 />
                 <link rel="icon" href="/favicon16.jpg" />
             </Head>
-            <main>
-                <p>Register page</p>
+            <main className={styles.main_page}>
+                <Card className={styles.register_card}>
+                    <Card.Body>
+                        <Card.Title>Registration</Card.Title>
+                        <Form>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="exampleForm.ControlInput1"
+                            >
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="name@example.com"
+                                />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label htmlFor="inputPassword5">
+                                    Password
+                                </Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    id="inputPassword5"
+                                    aria-describedby="passwordHelpBlock"
+                                />
+                                <Form.Text id="passwordHelpBlock" muted>
+                                    Your password must be min. 8 characters
+                                    long, contain letters, capital letters,
+                                    numbers and special characters.
+                                </Form.Text>
+                            </Form.Group>
+                        </Form>
+
+                        <Button variant="primary">Register</Button>
+                    </Card.Body>
+                </Card>
             </main>
         </div>
     );
