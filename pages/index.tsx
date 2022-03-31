009aa90781312d3ea3c983a39f78b62c8d9b931d
allobrox <rigoczkitamas@gmail.com>
2022-03-31 21:27:26 +0200

    MON-12: Added main illustration to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Index.module.css";
 
+import SteppingUp from "../public/illustration/undraw_stepping_up_g6oo.svg";
 import { IndexNavbar } from "../components/navbar/IndexNavbar/IndexNavbar";
 
 const Home: NextPage = () => {
@@ -15,7 +16,31 @@ const Home: NextPage = () => {
 
             <main className={styles.background}>
                 <IndexNavbar />
-                <div className={styles.text}>Index page</div>
+                <div className={styles.big_illustration_wrapper}>
+                    <div className={styles.big_illustration_text_column}>
+                        <div className={styles.big_illustration_title}>
+                            monetio
+                        </div>
+                        <div className={styles.big_illustration_subtitle}>
+                            the free, opensource budgeting app
+                        </div>
+                        <div className={styles.big_illustration_explanation}>
+                            Operating costs payed by donations, source code
+                            available at Github{" "}
+                        </div>
+                    </div>
+                    <div className={styles.big_i_wrapper}>
+                        <img
+                            className={styles.big_illustration}
+                            src="/illustration/undraw_stepping_up_g6oo.svg"
+                            alt="An SVG of an eye"
+                        />
+                        {/*<SteppingUp*/}
+                        {/*    className={styles.big_illustration}*/}
+                        {/*    fill={"#dad7dc"}*/}
+                        {/*/>*/}
+                    </div>
+                </div>
             </main>
         </div>
     );
