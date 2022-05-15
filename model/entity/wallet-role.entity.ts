85d2efd6394f71921c655a8658675204b7e5d541
allobrox <rigoczkitamas@gmail.com>
2022-05-15 15:44:53 +0200

    MON-11: Added WalletRole entity

---
+++ model/entity/wallet-role.entity.ts
@@ -0,0 +1,24 @@
+import { getByName, UserRole } from "../enum/user-role";
+
+export interface WalletRole {
+    id?: string;
+    userId?: string;
+    userRole?: UserRole;
+    walletId?: string;
+}
+
+export interface DatabaseWalletRole {
+    id?: string;
+    user_id?: string;
+    user_role: number;
+    wallet_id?: string;
+}
+
+export function convertWalletRole(dbEntity: DatabaseWalletRole): WalletRole {
+    return {
+        id: dbEntity.id,
+        userId: dbEntity.id,
+        userRole: getByName(UserRole[dbEntity.user_role]),
+        walletId: dbEntity.wallet_id
+    };
+}
