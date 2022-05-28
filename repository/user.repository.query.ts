b502bb4c334279653c5fbd245e1b6168279e46d4
allobrox <rigoczkitamas@gmail.com>
2022-05-28 13:19:36 +0200

    MON-23: Added email verification to use repository

--- repository/user.repository.query.ts
+++ repository/user.repository.query.ts
@@ -16,3 +16,14 @@ export function createUser(entity: User): QueryConfig {
         ]
     };
 }
+
+export function setUserVerified(
+    userId: string,
+    modifiedAt: number
+): QueryConfig {
+    return {
+        name: "UPDATE verified on USER",
+        text: "UPDATE users SET verified=true, modified_at=$2 WHERE id=$1;",
+        values: [userId, modifiedAt]
+    };
+}
