b3462afef93202e3cdd4feb4ade79aaa481632b4
allobrox <rigoczkitamas@gmail.com>
2022-05-29 09:58:16 +0200

    MON-13: Integrated Tedis into claims cache repository

--- core/cache-repository/claims.cache-repository.ts
+++ core/cache-repository/claims.cache-repository.ts
@@ -1,12 +1,17 @@
 import { Claims } from "../../model/dto/claims.dto";
 import { get, set } from "../service/redis-cache.service";
+import { Tedis } from "tedis";
+import { redisClientOptions } from "../redis-client-options";
 
 export async function getClaimsByEmail(email: string): Promise<Claims> {
     return await get(`claims:${email}`);
 }
 
 export async function getClaimsByUserId(userId: string): Promise<Claims> {
-    return await get(`claims:${userId}`);
+    const client = new Tedis(redisClientOptions);
+    const claimsString = await client.get(`claims:${userId}`);
+    client.close();
+    return JSON.parse(String(claimsString));
 }
 
 export async function putClaimsByEmail(
