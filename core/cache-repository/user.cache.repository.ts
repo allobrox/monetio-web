69234368b74000caf640cfb32297e22cd2fee296
allobrox <rigoczkitamas@gmail.com>
2022-05-29 09:58:36 +0200

    MON-13: Integrated Tedis into user cache repository

--- core/cache-repository/user.cache.repository.ts
+++ core/cache-repository/user.cache.repository.ts
@@ -1,8 +1,14 @@
 import { User } from "../../model/entity/user.entity";
 import { get, set } from "../service/redis-cache.service";
+import { Tedis } from "tedis";
+import { redisClientOptions } from "../redis-client-options";
 
 export async function getUserByEmail(email: string): Promise<User> {
-    return await get(`user:${email}`);
+    const client = new Tedis(redisClientOptions);
+    const userString = await client.get(`user:${email}`);
+    const user: User = JSON.parse(String(userString));
+    client.close();
+    return user;
 }
 
 export async function putUserByEmail(user: User, email: string): Promise<void> {
